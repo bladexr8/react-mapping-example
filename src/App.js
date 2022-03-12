67eb2074cb720874b61731643d762530ab7ad852
Stephen Moss <bladexr8@gmail.com>
2022-03-13 09:33:47 +1100

    mapbox tile layer

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
-//import LeafletMap from './components/leaflet-map';
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import LeafletMap from './components/leaflet-map';
 
 
 function App() {
   return (
     <div className="App">
-      <MapContainer center={[51.505, -0.09]} zoom={13}>
-
-<TileLayer
-    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-/>
-
-<Marker position={[51.505, -0.09]}>
-
-    <Popup>
-        A pretty CSS3 popup. < br /> Easily customizable.
-    </Popup>
-
-</Marker>
-
-
-</MapContainer>
+      <LeafletMap />
     </div>
   );
 }
--- src/components/leaflet-map.js
+++ src/components/leaflet-map.js
@@ -1,13 +1,42 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
-// Leaftlet Mapping Component
-function LeafletMap() {
-    return (
-        <MapContainer center={[51.505, -0.09]} zoom={13}>
+/*
 
-            <TileLayer
+OpenStreet Map Tile Layer
+
+<TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+
+*/
+
+/* Mapbox TileLayer definition */
+const layerDef = {
+    url: 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: process.env.REACT_APP_MAPBOX_TOKEN,
+}
+
+
+// Leaftlet Mapping Component
+function LeafletMap() {
+    console.log(layerDef);
+    return (
+        <MapContainer center={[-37.68046762370196, 144.5923961371349]} zoom={13}>
+
+            <TileLayer
+                url={layerDef.url}
+                attribution={layerDef.attribution}
+                maxZoom={layerDef.maxZoom}
+                id={layerDef.id}
+                tileSize={layerDef.tileSize}
+                zoomOffset={layerDef.zoomOffset}
+                accessToken={layerDef.accessToken}
             />
 
             <Marker position={[51.505, -0.09]}>
